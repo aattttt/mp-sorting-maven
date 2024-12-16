99242a9c9576577d0e6421596a14f4c173ec3c39
aattttt <atrimble2525@gmail.com>
2024-12-16 12:47:13 -0600

    SelectionSorter helper method added

--- src/main/java/edu/grinnell/csc207/sorting/SelectionSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/SelectionSorter.java
@@ -5,72 +5,69 @@ import java.util.Comparator;
 /**
  * Something that sorts using selection sort.
  *
- * @param <T>
- *   The types of values that are sorted.
+ * @param <T> The types of values that are sorted.
  *
  * @author Samuel A. Rebelsky
  * @author A.J. Trimble
  */
-
 public class SelectionSorter<T> implements Sorter<T> {
-  // +--------+------------------------------------------------------
-  // | Fields |
-  // +--------+
+    // +--------+------------------------------------------------------
+    // | Fields |
+    // +--------+
 
-  /**
-   * The way in which elements are ordered.
-   */
-  Comparator<? super T> order;
+    /**
+     * The way in which elements are ordered.
+     */
+    Comparator<? super T> order;
 
-  // +--------------+------------------------------------------------
-  // | Constructors |
-  // +--------------+
+    // +--------------+------------------------------------------------
+    // | Constructors |
+    // +--------------+
+    /**
+     * Create a sorter using a particular comparator.
+     *
+     * @param comparator The order in which elements in the array should be
+     * ordered after sorting.
+     */
+    public SelectionSorter(Comparator<? super T> comparator) {
+        this.order = comparator;
+    } // SelectionSorter(Comparator)
 
-  /**
-   * Create a sorter using a particular comparator.
-   *
-   * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
-   */
-  public SelectionSorter(Comparator<? super T> comparator) {
-    this.order = comparator;
-  } // SelectionSorter(Comparator)
-
-  // +---------+-----------------------------------------------------
-  // | Methods |
-  // +---------+
-
-  /**
-   * Sort an array in place using selection sort.
-   *
-   * @param values
-   *   an array to sort.
-   *
-   * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
-   */
-  @Override
-  public void sort(T[] values) {
-    if (values.length != 0) {
-      T temp;
-      for (int i = 0; i < values.length; i++) {
-        int min = i;
-        for (int j = i + 1; j < values.length; j++) {
-          if (this.order.compare(values[j], values[min]) < 0) {
-              min = j;
-          }
+    // +---------+-----------------------------------------------------
+    // | Methods |
+    // +---------+
+    /**
+     * Sort an array in place using selection sort.
+     *
+     * @param values an array to sort.
+     *
+     * @post The array has been sorted according to some order (often one given
+     * to the constructor).
+     * @post For all i, 0 &lt; i &lt; values.length, order.compare(values[i-1],
+     * values[i]) &lt;= 0
+     */
+    @Override
+    public void sort(T[] values) {
+        if (values.length != 0) {
+            T temp;
+            for (int i = 0; i < values.length; i++) {
+                int smallest = selectSmallest(values, i);
+                if (smallest != i) {
+                    temp = values[i];
+                    values[i] = values[smallest];
+                    values[smallest] = temp;
+                }
+            }
         }
-        if (min != i) {
-          temp = values[i];
-          values[i] = values[min];
-          values[min] = temp;
+    } // sort(T[])
+
+    private int selectSmallest(T[] values, int start) {
+        int smallest = start;
+        for (int i = start + 1; i < values.length; i++) {
+            if (this.order.compare(values[i], values[smallest]) < 0) {
+                smallest = i;
+            }
         }
-      }
-    }
-  } // sort(T[])
+        return smallest;
+    } // selectSmallest(T[], int)
 } // class SelectionSorter
